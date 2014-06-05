de6cf640868c1a204926f5c87a8a57c7582faebf
Philippe Gauthier <philippe@philippe.io>
2014-06-05 13:50:59 -0700

    changed landing page border height and width

--- _/components/js/scripts.js
+++ _/components/js/scripts.js
@@ -112,8 +112,8 @@ $(document).ready(function(){
 });
 
 $(document).ready(function(){
-  var headerHeight = $('.fixed-header-text').height() + 20;
-  $('.portfolio-content').css("padding-top",headerHeight+'px');
+  var headerHeight = $('.fixed-header-text').height();
+  $('.portfolio-content').css("margin-top",headerHeight+'px');
 });
 
 
