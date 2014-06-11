7854acdd4ecffe89ec18b109bba24333907c3f9b
Philippe Gauthier <philippe@philippe.io>
2014-06-11 16:11:13 -0700

    test

--- statamic/_themes/main/js/main.js
+++ statamic/_themes/main/js/main.js
@@ -2164,6 +2164,11 @@ $.magnificPopup.registerModule(RETINA_NS, {
 
 // Google Analytics
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-17194742-1']);
+  _gaq.push(['_setDomainName', 'adweisman.com']);
+  _gaq.push(['_trackPageview']);
+
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
